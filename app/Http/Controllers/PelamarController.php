1939ab3671f2cb5d9a8cb7cb3546886d271f5b35
SIMPLIX07 <muhammad99salman98@gmail.com>
2026-01-04 23:02:13 +0700

    a

--- app/Http/Controllers/PelamarController.php
+++ app/Http/Controllers/PelamarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pelamar;
 use App\Models\Keahlian;
+use GuzzleHttp\RedirectMiddleware;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
