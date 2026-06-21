bb7ab3eac2c12fbd29233fa518e367bba0f1c25a
SIMPLIX07 <muhammad99salman98@gmail.com>
2026-06-21 20:20:30 +0700

    menambahkan wbt salman

--- automation testing/WBT/Salman/Membuat Lowongan/R3AksesTanpaLoginTest.php
+++ automation testing/WBT/Salman/Membuat Lowongan/R3AksesTanpaLoginTest.php
@@ -5,12 +5,12 @@ namespace Tests\WBT\Salman\MembuatLowongan;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class R5AksesTanpaLoginTest extends TestCase
+class R3AksesTanpaLoginTest extends TestCase
 {
     use RefreshDatabase;
 
     /**
-     * WBT R5: Gagal membuat lowongan / dialihkan karena akses tanpa login perusahaan
+     * WBT R3: Gagal membuat lowongan / dialihkan karena akses tanpa login perusahaan
      */
     public function test_store_lowongan_akses_tanpa_login(): void
     {
